33639146752b155917219babf3b181ddd15f7522
alexanderutrobin11 <alexanderutrobin11@gmail.com>
2019-03-28 17:50:51 +0300

    Updates

---
+++ precache-manifest.a14cf0e899b33853503d14eab87b41da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d5d1ee162a4e12c0ac9b9324a8bed5b",
+    "url": "/cv/static/media/photo.6d5d1ee1.jpeg"
+  },
+  {
+    "revision": "18079a4bdaee1d56978c",
+    "url": "/cv/static/js/runtime~main.6f651ed7.js"
+  },
+  {
+    "revision": "4ddce5c8c3d6e448412e",
+    "url": "/cv/static/js/main.30c2bb34.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/cv/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "4ddce5c8c3d6e448412e",
+    "url": "/cv/static/css/main.85d65342.chunk.css"
+  },
+  {
+    "revision": "05de3082b34e619fc888f90aeef94b34",
+    "url": "/cv/index.html"
+  }
+];
